86849a72e566c412f20865bb185ce9ffc7d656b1
Sir_Jeager <46685568+Jeagermeister@users.noreply.github.com>
2021-12-03 14:12:49 -0600

    Add files via upload

--- RemoveDuplicates.py
+++ RemoveDuplicates.py
@@ -3,3 +3,12 @@
 # Title: Removing Duplicates from an array
 
 # begin here
+num_list = [1, 3, 0, 1, 9, -1, 9, 8, 7]
+new_list = []
+
+for i in num_list:
+    if i not in new_list:
+        new_list.append(i)
+        
+print(str(num_list))
+print(str(new_list))
